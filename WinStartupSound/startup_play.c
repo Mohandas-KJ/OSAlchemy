6e53b55f996e6c5fe05ef409a3c74c4c19bfad64
Mohan Das <mohandaskj.11@gmail.com>
2026-05-04 20:15:47 +0530

    Implement waitForAudioService function to monitor audio service status

--- WinStartupSound/startup_play.c
+++ WinStartupSound/startup_play.c
@@ -2,3 +2,50 @@
 #include <stdio.h>
 #include <windows.h>
 
+//Linkers
+#pragma comment(lib,"winmm.lib")
+#pragma comment(lib, "advapi32.lib")
+
+//Function to Check Constantly
+int waitForAudioService(){
+    
+    //Connect to SCM Database
+    SC_HANDLE scm = OpenSCManager(NULL,NULL,SC_MANAGER_CONNECT);
+
+    //Verify
+    if(!scm){
+        return 0;
+    }
+
+    //Search for a query
+    SC_HANDLE service = OpenService(scm,"Audiosrv",SERVICE_QUERY_STATUS);
+
+    //Verify
+    if (!service){
+        CloseServiceHandle(scm);
+        return 0;
+    }
+
+    //DWORD to save status
+    SERVICE_STATUS status;
+
+    //Check constantly
+    while (1){
+        QueryServiceStatus(service,&status);
+
+        if(status.dwCurrentState == SERVICE_RUNNING){
+            break;
+        }
+
+        Sleep(1000);
+    }
+    
+    CloseServiceHandle(scm);
+    CloseServiceHandle(service);
+    return 1;
+
+}
+
+int main(){
+    return 0;
+}
